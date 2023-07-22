62b246a1cd9a54ab46a6f21d12d76c915a608350
Anubhav Rakshit <anubhav.rakshit@gmail.com>
2023-07-22 22:49:19 +0530

    first commit

---
+++ day_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int max_cal_sofar{0}, current_cal{0};
+    string input;
+    vector<int> cals;   
+    while (getline(cin, input)) {
+        if (!input.length()) {
+            cals.push_back(current_cal);
+            current_cal = 0;
+            continue;
+        }
+        stringstream ss(input);
+        int c;
+        ss >> c;
+        current_cal += c;
+        max_cal_sofar = max(max_cal_sofar, current_cal);
+    }
+    // Last calorie value needs to be accounted for
+    cals.push_back(current_cal);
+    std::cout << "Max cal " << max_cal_sofar << endl;
+    sort(cals.begin(), cals.end(), greater());
+    std::cout << "Top 3 cal sum " << cals[0] + cals[1] + cals[2] << endl;
+    return 0;
+}
---
+++ day_2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <sstream>
+
+enum game_move {R = 'A', P = 'B', S = 'C', RO = 'X', PA = 'Y', SI = 'Z'};
+using namespace std;
+int play(game_move opp, game_move you) {
+    map <game_move, int> shape_score;
+    shape_score[RO] = 1;
+    shape_score[PA] = 2;
+    shape_score[SI] = 3;
+
+    map <pair<game_move, game_move>, int> outcome;
+    // Draw 3 points
+    outcome[{R, RO}] = 3;
+    outcome[{P, PA}] = 3;
+    outcome[{S, SI}] = 3;
+    // You loose 0 point
+    outcome[{R, SI}] = 0;
+    outcome[{P, RO}] = 0;
+    outcome[{S, PA}] = 0;
+    // You win 6 points
+    outcome[{R, PA}] = 6;
+    outcome[{P, SI}] = 6;
+    outcome[{S, RO}] = 6;
+    
+    return  outcome[{opp, you}] + shape_score[you];
+}
+
+int choose_move(game_move opp, game_move you) {
+    map<game_move, game_move> loose, win, draw;
+    loose[R] = SI;
+    loose[P] = RO;
+    loose[S] = PA;
+    draw[R] = RO;
+    draw[P] = PA;
+    draw[S] = SI;
+    win[R] = PA;
+    win[P] = SI;
+    win[S] = RO;
+    
+    if (you == RO) {
+        // Loose
+        cout << "Loose" << endl;
+        return play(opp, loose[opp]);
+    } else if (you == PA) {
+        // Draw
+        cout << "Draw" << endl;
+        return play(opp, draw[opp]);
+    } else {
+        // Win
+        cout << "Win" << endl;
+        return play(opp, win[opp]);
+    }
+}
+
+int main() {
+    int total_score{0};
+    string input;
+    while (getline(cin, input)) {
+        stringstream ss(input);
+        char opp, you;
+        ss >> opp >> you;
+        int score = choose_move(static_cast<game_move>(opp), static_cast<game_move>(you));
+        cout << "Score " << score << endl;
+        total_score += score;
+    }
+
+    cout << "Total Score " << total_score << endl;
+    return 0;
+}
