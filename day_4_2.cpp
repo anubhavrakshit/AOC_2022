d958f66ee23c0ca4beb3f27ac1cee3c953dc0fb8
Anubhav Rakshit <anubhav.rakshit@gmail.com>
2023-07-27 20:44:39 +0530

    Day 4 puzzles

---
+++ day_4_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+    string line;
+    int num_overlaps{0};
+    while (getline(cin, line)) {
+        stringstream ss(line);
+        int l1, h1, l2, h2;
+        char dash, comma;
+        ss >> l1 >> dash >> h1 >> comma >> l2 >> dash >> h2;
+        cout << "Range" << l1 << "-" << h1 << "," << l2 << "-" << h2 << endl;
+        if (l1 <= l2 and l2 >= l1 and l2 <= h1) {
+            num_overlaps++;
+        } else if (l2 <= l1 and l1 >= l2 and l1 <= h2 ) {
+            num_overlaps++;
+        }
+    }
+    cout << "Num Overlaps " << num_overlaps << endl;
+    return 0;
+}
