c00db5f6017e7f27e1cfbac64bd132137a334ac0
Anubhav Rakshit <anubhav.rakshit@gmail.com>
2023-07-30 17:26:34 +0530

    Day 6 puzzles

---
+++ day_6_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <unordered_map> 
+using namespace std;
+bool no_dups(unordered_map<char, int>& win_count) {
+    for (pair<char, int> elems : win_count) {
+        if (elems.second > 1) {
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+    string line;
+    const int win_size{14};
+    while (getline(cin, line)) {
+        int l{0}, r{0};
+        unordered_map<char, int> win_count;
+        while (r < line.length()) {
+            if (r < win_size) {
+                win_count[line[r]]++;
+                r++;
+            } else {
+                if (no_dups(win_count)) {
+                    cout << r << endl;
+                    break;
+                }
+                win_count[line[l]]--;
+                l++;
+                win_count[line[r]]++;
+                r++;
+            }
+        }
+    }
+    return 0;
+}
