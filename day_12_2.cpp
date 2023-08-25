685b987cbf980398adc8ea585c968b3c45efe305
Anubhav Rakshit <anubhav.rakshit@gmail.com>
2023-08-25 19:34:19 +0530

    Refactor bfs code

--- day_12_2.cpp
+++ day_12_2.cpp
@@ -10,7 +10,7 @@ using row = vector<char>;
 using grid = vector<row>;
 using pos = pair<int, int>;
 
-int do_bfs(grid &heightmap, pos startpos) {
+int do_bfs(grid &heightmap, pos startpos, pos endpos) {
   set<pos> visit;
   int min_dist{INT_MAX};
   // qentry will contain pos, distance
@@ -28,23 +28,19 @@ int do_bfs(grid &heightmap, pos startpos) {
     auto entry_dis = entry.second;
 
     // Reached Endpos
-    if (heightmap[entry_pos.first][entry_pos.second] == 'E') {
+    if (entry_pos == endpos) {
       min_dist = entry_dis;
       break;
     }
 
     // Start Pos has height of a
-    int curheight = heightmap[entry_pos.first][entry_pos.second] == 'S'
-                        ? 'a'
-                        : heightmap[entry_pos.first][entry_pos.second];
-
+    int curheight = heightmap[entry_pos.first][entry_pos.second];
     auto is_valid_pos = [&](int i, int j) {
       if (i < 0 or j < 0 or i >= nrows or j >= ncols) {
         return false;
       }
 
-      // End Pos has height of z
-      int nextheight = heightmap[i][j] == 'E' ? 'z' : heightmap[i][j];
+      int nextheight = heightmap[i][j];
       if (nextheight - curheight == 1 or curheight >= nextheight) {
         return true;
       }
@@ -53,7 +49,6 @@ int do_bfs(grid &heightmap, pos startpos) {
 
     // Get all valid next positions
     vector<pair<int, int>> alldirs = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
-    cout << "Explore dist " << entry_dis << endl;
     for (auto &dir : alldirs) {
       pos next_pos = {entry_pos.first + dir.first,
                       entry_pos.second + dir.second};
@@ -83,24 +78,26 @@ int main() {
   int nrows = heightmap.size();
   int ncols = heightmap[0].size();
   cout << "Num rows : cols  " << nrows << " : " << ncols << endl;
-#if 0 
-     for (auto r : heightmap) {
-        for (auto c : r) {
-            cout << c << " ";
-        }
-        cout << endl;
-     }
-#endif
+
   // Find the start pos
-  pos startpos;
+  vector<pos> all_startpos;
+  pos endpos;
   int min_dis{INT_MAX};
   for (int i = 0; i < nrows; i++) {
     for (int j = 0; j < ncols; j++) {
       if (heightmap[i][j] == 'S' or heightmap[i][j] == 'a') {
-        startpos = {i, j};
-        min_dis = min(do_bfs(heightmap, startpos), min_dis);
+        // height of S is a
+        heightmap[i][j] = 'a';
+        all_startpos.push_back({i, j});
+      } else if (heightmap[i][j] == 'E') {
+        heightmap[i][j] = 'z';
+        endpos = {i, j};
       }
     }
+  }
+
+  for (auto startpos : all_startpos) {
+    min_dis = min(do_bfs(heightmap, startpos, endpos), min_dis);
   }
 
   cout << "Min steps " << min_dis << endl;
